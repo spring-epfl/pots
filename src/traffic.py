448f89e682e2f127be96fcf226d9d81162bc1b00
Bogdan Kulynych <hello@bogdankulynych.me>
2019-08-13 14:34:21 +0200

    Fix a bug in math; remove LP

--- src/traffic.py
+++ src/traffic.py
@@ -38,6 +38,11 @@ def get_time_delta(edge_data, target_speed_lim=MIN_SPEED_LIM):
     return edge_data.length / target_speed_lim - edge_data.get_time()
 
 
+def get_time_delta(edge_data, target_speed_lim=MIN_SPEED_LIM):
+    speed_delta = edge_data.speed_lim - target_speed_lim
+    return edge_data.length * speed_delta / (edge_data.speed_lim**2 - edge_data.speed_lim * speed_delta)
+
+
 def dot(x, y):
     res = x[0] * y[0]
     for a, b in zip(x, y):
@@ -190,7 +195,7 @@ def solve_milp(
         time_delta = get_time_delta(edge_data, target_speed_lim=updated_speed_lim)
         arc_time_deltas[edge] = time_delta
         if updated_speed_lim >= MIN_SPEED_LIM:
-            solver.Add(q - p - interdiction_var * time_delta <= edge_data.get_time())
+            solver.Add(q - p <= edge_data.get_time() + interdiction_var * time_delta)
         else:
             solver.Add(q - p <= edge_data.get_time())
 
@@ -223,66 +228,6 @@ def solve_milp(
     # print('Problem solved in %d branch-and-bound nodes' % solver.nodes())
 
 
-def solve_lp(nodes, edges, min_time, source="S", target="T", verbose=False):
-    # Initialize solver.
-    solver = pywraplp.Solver("waze", pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-
-    # Constants.
-    interdiction_costs = [1 for _ in edges]
-
-    # Initialize interdiction variables.
-    interdiction_vars = []
-    interdiction_vars_by_arc = {}
-    for i, (edge, edge_data) in enumerate(edges.items()):
-        upper_lim = get_time_delta(edge_data)
-        var = solver.NumVar(0.0, upper_lim, "x%i" % i)
-        interdiction_vars.append(var)
-        interdiction_vars_by_arc[edge] = var
-
-    # Initialize shortest-path variables.
-    shortest_path_vars = []
-    path_vars_by_node = {}
-    for i, node in enumerate(nodes):
-        var = solver.NumVar(-solver.infinity(), solver.infinity(), "p%i" % i)
-        shortest_path_vars.append(var)
-        path_vars_by_node[node] = var
-
-    # Specify constraints.
-    solver.Add(path_vars_by_node[target] - path_vars_by_node[source] >= min_time)
-
-    for edge, edge_data in edges.items():
-        interdiction_var = interdiction_vars_by_arc[edge]
-        p = path_vars_by_node[edge[0]]
-        q = path_vars_by_node[edge[1]]
-        solver.Add(q - p - interdiction_var <= edge_data.get_time())
-
-    # Specify the objective.
-    solver.Minimize(dot(interdiction_costs, interdiction_vars))
-    result_status = solver.Solve()
-    assert result_status == pywraplp.Solver.OPTIMAL
-
-    solution = []
-    for edge, var in interdiction_vars_by_arc.items():
-        time_delta = var.solution_value()
-        edge_data = edges[edge]
-        if get_speed_delta(edge_data, time_delta) < -TOLERANCE:
-            solution.append((edge, edge_data, get_speed_delta(edge_data, time_delta)))
-            if verbose:
-                print(
-                    "Road: %s - %2.2f miles." % (repr(edge), edge_data.length),
-                    "Original speed lim: %2.2f -> New speed lim: %2.2f"
-                    % (
-                        edge_data.speed_lim,
-                        edge_data.speed_lim + get_speed_delta(edge_data, time_delta),
-                    ),
-                )
-
-    return solution
-
-    # print('Problem solved in %f milliseconds' % solver.wall_time())
-    # print('Problem solved in %d iterations' % solver.iterations())
-
-
 def adaptive_delta(edge_data):
     speed_lim = edge_data.speed_lim
     if speed_lim <= 30:
@@ -309,4 +254,6 @@ if __name__ == "__main__":
         costs="length",
         verbose=True,
     )
-    print("Time: %2.2f ms" % time)
+    print("Solution size:", len(solution))
+    print("Solving time: %2.2f ms" % time)
+
