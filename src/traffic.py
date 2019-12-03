9c58b83e0d7cad77497974e5c5bd2855bb797c2f
Bogdan Kulynych <hello@bogdankulynych.me>
2019-12-03 22:51:09 +0100

    Revert "Split waze file into a script and library"
    This reverts commit 7a6424d732f1bf0bad1def682bbfe6c631cc2fa4.

--- src/traffic.py
+++ src/traffic.py
@@ -1,23 +1,62 @@
-#! /usr/bin/env python
-
-# CLI for running Waze experiments.
-
 # Units of time: hours
 # Units of distance: miles
 
-import attr
+from pyroutelib3 import Router
 import click
+import sys
+import attr
 import osmnx as ox
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-from pyroutelib3 import Router
+from sklearn.neighbors import KDTree
+from datetime import datetime
 
-from src.traffic_solver import solve_milp, get_speed_delta, EdgeData, SolutionNotFound
+from ortools.linear_solver import pywraplp
+
+# Minimum possible speed limit in mph.
+MIN_SPEED_LIM = 15
+
+TOLERANCE = 0.001
 
 
 def meters_to_miles(meters):
     return meters * 0.0006213712
+
+
+class SolutionNotFound(Exception):
+    pass
+
+
+@attr.s
+class EdgeData:
+    """Data associated with an edge in a traffic network."""
+
+    INTERSECTION_DELAY = 7 / 3600
+
+    speed_lim = attr.ib()
+    length = attr.ib()
+
+    def get_time(self):
+        return self.length / self.speed_lim + EdgeData.INTERSECTION_DELAY
+
+
+def get_speed_delta(edge_data, target_time_delta):
+    """What is the minimum speed limit delta that would make this edge slower by delta"""
+    return (
+        target_time_delta
+        * edge_data.speed_lim ** 2
+        / (target_time_delta * edge_data.speed_lim + edge_data.length)
+    )
+
+
+def get_time_delta(edge_data, target_speed_lim=MIN_SPEED_LIM):
+    speed_delta = edge_data.speed_lim - target_speed_lim
+    return (
+        edge_data.length
+        * speed_delta
+        / (edge_data.speed_lim ** 2 - edge_data.speed_lim * speed_delta)
+    )
 
 
 def set_speed(type, d):
@@ -33,24 +72,6 @@ def set_speed(type, d):
         return 55
     else:
         return 25
-
-
-def update_speedlimits(G_town, origin, dest, solution):
-    for s in solution:
-        su = s[0][0]
-        sv = s[0][1]
-        if su == "T":
-            su = dest
-        if sv == "T":
-            sv = dest
-        if su == "S":
-            su = origin
-        if sv == "S":
-            sv = origin
-
-        for u, v, d in G_town.edges(data=True):
-            if u in (su, sv) and v in (su, sv):
-                d["speed_limit"] = d["speed_limit"] + s[2]
 
 
 def get_town_data(town_params):
@@ -92,6 +113,120 @@ def get_town_data(town_params):
             processed_edges[u, v] = e
     G = ox.gdfs_to_graph(nodes, edges)
     return (G, town_params.source, town_params.dest), (processed_nodes, processed_edges)
+
+
+def dot(x, y):
+    res = x[0] * y[0]
+    for a, b in zip(x, y):
+        res += a * b
+    return res
+
+
+def solve_milp(
+    nodes,
+    edges,
+    min_time,
+    source="S",
+    target="T",
+    costs="uniform",
+    delta=5,
+    verbose=False,
+):
+    """
+    Solve MILP for traffic interdiction.
+
+    Args:
+        nodes: List of node identifiers
+        edges (dict): Traphic graph arcs. Mapping of node tuples that represent node
+                identifiers (a, b) to :py:class:`Edge` objects.
+        min_time: Minimum time to pass through town in hours
+        delta: Speed limit delta for any interdiction.
+        costs: Cost per interdiction. Either an iterable of costs, or one of ["uniform", "length"].
+        verbose: Output the solutions.
+    """
+    if isinstance(delta, int) or isinstance(delta, float):
+        delta_scheduler = lambda *args, **kwargs: delta
+    else:
+        delta_scheduler = delta
+
+    # Initialize solver.
+    solver = pywraplp.Solver("waze_milp", pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+
+    # Constants.
+    if costs == "uniform":
+        interdiction_costs = [1 for _ in edges]
+    elif costs == "length":
+        interdiction_costs = [edge_data.length for edge_data in edges.values()]
+    else:
+        interdiction_costs = costs
+
+    # Initialize interdiction variables.
+    interdiction_vars = []
+    interdiction_vars_by_arc = {}
+    for i, (edge, edge_data) in enumerate(edges.items()):
+        var = solver.IntVar(0.0, 1.0, "x%i" % i)
+        interdiction_vars.append(var)
+        interdiction_vars_by_arc[edge] = var
+
+    # Initialize shortest-path variables.
+    shortest_path_vars = []
+    path_vars_by_node = {}
+    for i, node in enumerate(nodes):
+        var = solver.NumVar(-solver.infinity(), solver.infinity(), "p%i" % i)
+        shortest_path_vars.append(var)
+        path_vars_by_node[node] = var
+
+    # Specify constraints.
+    solver.Add(path_vars_by_node[target] - path_vars_by_node[source] >= min_time)
+
+    arc_time_deltas = {}
+    for edge, edge_data in edges.items():
+        interdiction_var = interdiction_vars_by_arc[edge]
+        p = path_vars_by_node[edge[0]]
+        q = path_vars_by_node[edge[1]]
+
+        updated_speed_lim = edge_data.speed_lim - delta_scheduler(edge_data)
+        time_delta = get_time_delta(edge_data, target_speed_lim=updated_speed_lim)
+        arc_time_deltas[edge] = time_delta
+        if updated_speed_lim >= MIN_SPEED_LIM:
+            solver.Add(q - p <= edge_data.get_time() + interdiction_var * time_delta)
+        else:
+            solver.Add(q - p <= edge_data.get_time())
+
+    # Specify the objective.
+    print("Solving... %s" % (datetime.now()))
+    solver.Minimize(dot(interdiction_costs, interdiction_vars))
+    result_status = solver.Solve()
+    if result_status != pywraplp.Solver.OPTIMAL:
+        raise SolutionNotFound()
+
+    print("Solved %s" % (datetime.now()))
+    solution = []
+    for edge, var in interdiction_vars_by_arc.items():
+        time_delta = var.solution_value() * arc_time_deltas[edge]
+        edge_data = edges[edge]
+        if get_speed_delta(edge_data, time_delta) >= TOLERANCE:
+            solution.append((edge, edge_data, get_speed_delta(edge_data, time_delta)))
+            # edge_data['delta'] = edge_data.speed_lim + get_speed_delta(edge_data, time_delta)
+            if verbose:
+                print(
+                    "Road: %s - %2.2f miles." % (repr(edge), edge_data.length),
+                    "Original speed lim: %2.2f -> New speed lim: %2.2f"
+                    % (
+                        edge_data.speed_lim,
+                        edge_data.speed_lim - get_speed_delta(edge_data, time_delta),
+                    ),
+                )
+    print(
+        "solved with solution of len %d and cost %2.2f in %f seconds"
+        % (len(solution), solver.Objective().Value(), solver.wall_time())
+    )
+
+    return solution, solver.wall_time(), solver.Objective().Value()
+
+    # print('Problem solved in %f milliseconds' % solver.wall_time())
+    # print('Problem solved in %d iterations' % solver.iterations())
+    # print('Problem solved in %d branch-and-bound nodes' % solver.nodes())
 
 
 def print_graph(G_town, origin, dest, solution, title="map"):
@@ -169,13 +304,14 @@ def print_graph(G_town, origin, dest, solution, title="map"):
         save=True,
         close=True,
         file_format="pdf",
-        filename="images/maps/%s" % (title),
+        filename="maps/%s" % (title),
         show=False,
         route_linewidth=0,
         edge_linewidth=edges["edge_linewidth"],
     )
+    # print('changed: ' + str(changed))
 
-    # fig.savefig("maps/%s.png" % (title), dpi=1000, format="pdf")
+    # fig.savefig('maps/%s.png' % (title), dpi=1000, format='pdf')
 
 
 def speed_limit_change_by_15(edge_data):
@@ -197,7 +333,6 @@ def time_coef(edge_data, coef=0.3333):
 
 
 def all_pairs_cost(G_changed):
-    """Compute average cost between any two points in town."""
     indexable_edges = {}
     for u, v, d in G_changed.edges(data=True):
         d["weight"] = (d["length"]) / d["speed_limit"]
@@ -225,7 +360,7 @@ class TownParams:
     source = attr.ib()
 
 
-TOWN_PARAMS_MAP = {
+town_params_map = {
     "leonia": TownParams(town_name="Leonia, NJ", dest=103185965, source=4207193769),
     "lieusaint": TownParams(
         town_name="Lieusaint, France", source=1932540940, dest=1472509406
@@ -234,6 +369,24 @@ TOWN_PARAMS_MAP = {
         town_name="Fremont, California", source=52986461, dest=53006402
     ),
 }
+
+
+def update_speedlimits(G_town, origin, dest, solution):
+    for s in solution:
+        su = s[0][0]
+        sv = s[0][1]
+        if su == "T":
+            su = dest
+        if sv == "T":
+            sv = dest
+        if su == "S":
+            su = origin
+        if sv == "S":
+            sv = origin
+
+        for u, v, d in G_town.edges(data=True):
+            if u in (su, sv) and v in (su, sv):
+                d["speed_limit"] = d["speed_limit"] + s[2]
 
 
 @attr.s
@@ -267,7 +420,7 @@ def cli(ctx, town, delta, interdiction_cost, eval_cost):
 
     ctx.obj = ExperimentParams(
         town_code=town,
-        town_params=TOWN_PARAMS_MAP[town],
+        town_params=town_params_map[town],
         delta=delta,
         interdiction_cost=interdiction_cost,
         eval_cost=eval_cost,
@@ -318,11 +471,7 @@ def run_experiments(experiment_params, max_time, min_time, time_step):
                 # TODO: Is this right?
                 costs.append(milp_cost)
 
-    with open(
-        "images/%s/%s_town_effects.csv"
-        % (experiment_params.town_code, experiment_params.eval_cost),
-        "w",
-    ) as o:
+    with open("%s/%s_town_effects.csv" % (town, eval_cost), "w") as o:
         for t, c in zip(times, costs):
             if c == None:
                 c = -1
